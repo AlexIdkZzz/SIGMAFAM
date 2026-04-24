b205da2ed255901904027566ae99e87f35163b66
Yael <pingolillo@gmail.com>
2026-04-23 19:44:03 -0600

    Cambios en el diseno del sitio

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import Drawer from "./_drawer";
-import { useAlerts } from "../alerts/AlertsContext";
+import { useAlerts } from "../app/alerts/AlertsContext";
 import MiniMap from "../maps/MiniMap";
 import {
   Clock, Search, Filter, Eye, ChevronLeft, ChevronRight, 
